0c05656a2b1458455e00359e85f14739aff14f5c
Suraj-787 <1si23ad057@sit.ac.in>
2025-04-19 23:10:06 +0530

    chore: Update background color in hero scene component

--- src/components/hero-scene.tsx
+++ src/components/hero-scene.tsx
@@ -219,7 +219,7 @@ export function HeroScene() {
 
   return (
     <Canvas camera={{ position: [0, 0, 8], fov: 50 }}>
-      <color attach="background" args={["#f8fafc"]} />
+      <color attach="background" args={["#000000"]} />
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} intensity={1} />
       <spotLight position={[-10, -10, -10]} intensity={0.5} />
